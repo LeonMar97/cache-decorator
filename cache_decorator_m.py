c238985cb246cb8cd420a343fbfbe13d5d640c30
Leon Markovich <leonmarkovich97@gmail.com>
2023-07-27 18:36:29 +0300

    added decorator files..

---
+++ cache_decorator_m.py
@@ -0,0 +1,25 @@
+import hashlib
+def cache_decorator(fn):
+    cache = {}
+
+    def wrapper(*args ,use_cache=True, **kwargs):
+        nonlocal cache
+        args_str = ', '.join(str(arg) for arg in args)
+        kwargs_str = ', '.join(f"{key}={value}" for key, value in kwargs.items())
+        curFn = "{}({})".format(fn.__name__, ', '.join([args_str, kwargs_str]))
+        hash_key = hashlib.sha256(curFn.encode()).hexdigest()#im using hashing incase the function call gets very long
+
+        if hash_key in cache and use_cache==True :
+            print("this what ive found in cache :")
+            return cache[hash_key]
+        
+        else:
+            print("not in cache")
+            result=fn(*args,**kwargs)    
+            cache[hash_key]=result
+            return result    
+    return wrapper
+
+
+
+
---
+++ check_decorator.py
@@ -0,0 +1,24 @@
+from cache_decorator_m import cache_decorator
+
+@cache_decorator
+def greet(name):
+    return "hello {}".format(name)
+@cache_decorator
+def hello_world():
+    return "hello World!"
+@cache_decorator
+def calculate_pow(num,Pows):#very stupid pow calculation
+    sum=1
+    for i in range(Pows):
+        sum=sum*num
+    return sum
+if __name__=="__main__":
+    print(greet("michal"))
+    print(greet("nir"))
+    print(greet("michal",use_cache=False))
+    print(greet("nir"))
+    print(calculate_pow(5,7))
+    print(calculate_pow(5,7))
+    print(calculate_pow(5,8))
+    print(hello_world())
+    print(hello_world())
